a4f6aa31050610bf790436f2e0816c66fafcc3d4
lz <liangze@qingdan.com>
2024-07-14 20:02:21 +0800

    1st

---
+++ # Tools Configuration.md
@@ -0,0 +1,68 @@
+# Tools Configuration
+
+
+## Environment
+
+GOBIN: undefined
+toolsGopath: 
+gopath: /Users/tattoo1890/goWork
+GOROOT: /usr/local/go
+PATH: /Library/Frameworks/Python.framework/Versions/3.12/bin:/Library/Frameworks/Python.framework/Versions/3.6/bin:/usr/local/bin:/usr/local/sbin:/Library/Frameworks/Python.framework/Versions/3.8/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/go/bin:/Library/Apple/usr/bin:/Users/lz/Library/Android/sdk/tools:/Users/lz/Library/Android/sdk/platform-tools:/Library/Java/JavaVirtualMachines/jdk-1.8.jdk/Contents/Home/bin
+
+## Tools
+
+	go:	/usr/local/go/bin/go: go version go1.22.4 darwin/amd64
+
+	gopls:	not installed
+	gotests:	not installed
+	gomodifytags:	not installed
+	impl:	not installed
+	goplay:	not installed
+	dlv:	not installed
+	golint:	not installed
+
+## Go env
+
+Workspace Folder (dltw): /Users/lz/code/dltw
+
+	GO111MODULE='on'
+	GOARCH='amd64'
+	GOBIN=''
+	GOCACHE='/Users/lz/Library/Caches/go-build'
+	GOENV='/Users/lz/Library/Application Support/go/env'
+	GOEXE=''
+	GOEXPERIMENT=''
+	GOFLAGS=''
+	GOHOSTARCH='amd64'
+	GOHOSTOS='darwin'
+	GOINSECURE=''
+	GOMODCACHE='/Users/tattoo1890/goWork/pkg/mod'
+	GONOPROXY=''
+	GONOSUMDB=''
+	GOOS='darwin'
+	GOPATH='/Users/tattoo1890/goWork'
+	GOPRIVATE=''
+	GOPROXY='https://proxy.golang.org,direct'
+	GOROOT='/usr/local/go'
+	GOSUMDB='sum.golang.org'
+	GOTMPDIR=''
+	GOTOOLCHAIN='auto'
+	GOTOOLDIR='/usr/local/go/pkg/tool/darwin_amd64'
+	GOVCS=''
+	GOVERSION='go1.22.4'
+	GCCGO='gccgo'
+	GOAMD64='v1'
+	AR='ar'
+	CC='clang'
+	CXX='clang++'
+	CGO_ENABLED='1'
+	GOMOD='/Users/lz/code/dltw/go.mod'
+	GOWORK=''
+	CGO_CFLAGS='-O2 -g'
+	CGO_CPPFLAGS=''
+	CGO_CXXFLAGS='-O2 -g'
+	CGO_FFLAGS='-O2 -g'
+	CGO_LDFLAGS='-O2 -g'
+	PKG_CONFIG='pkg-config'
+	GOGCCFLAGS='-fPIC -arch x86_64 -m64 -pthread -fno-caret-diagnostics -Qunused-arguments -fmessage-length=0 -ffile-prefix-map=/var/folders/vh/zqhn6qkj1l57m6r8j2fsfn700000gn/T/go-build2998653260=/tmp/go-build -gno-record-gcc-switches -fno-common'
+	
---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"log"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/imroc/req/v3"
+)
+
+func handlerError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getInfo(id string) []string {
+	client := req.C()
+	request := client.R()
+
+	request.SetHeaders(map[string]string{
+		"accept": "*/*",
+		"accept-language": "zh-CN,zh;q=0.9",
+		"authorization": "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA",
+		"content-type": "application/json",
+		"priority": "u=1, i",
+		"referer": "https://x.com/Kunluntalk/status/1812256811199922205",
+		"sec-ch-ua": `"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"`,
+		"sec-ch-ua-mobile": "?0",
+		"sec-ch-ua-platform": "macOS",
+		"sec-fetch-dest": "empty",
+		"sec-fetch-mode": "cors",
+		"sec-fetch-site": "same-origin",
+		"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+		"x-client-transaction-id": "1HyuKuXbTS0Jl0IAJMEU4bRiQ3lA4LrVIw/QBTgpexcMDPar0QjgrpyZHvrkzl9eAIPHkNZRlflXMs5OGK1DNyzIwr2t1w",
+		"x-csrf-token": "36c6b644b7d47c3cf0c6dd20295d2f913385b6799f068aac210b4ea3cb3edb9baac6a582acf6c3f6b64fd996a632a6d0c579631e1788fd2f3d6dcd9e5c1a887e8c282c840215a1f5fbe8f1c6405aab8b",
+		"x-twitter-active-user": "yes",
+		"x-twitter-auth-type": "OAuth2Session",
+		"x-twitter-client-language": "zh-cn",
+	})
+
+	cookies := []*http.Cookie{
+		{Name: "guest_id", Value: "v1%3A172092576791049011"},
+		{Name: "night_mode", Value: "2"},
+		{Name: "guest_id_marketing", Value: "v1%3A172092576791049011"},
+		{Name: "guest_id_ads", Value: "v1%3A172092576791049011"},
+		{Name: "gt", Value: "1812380012072546732"},
+		{Name: "kdt", Value: "j8XVU8Vn3sEFrqz4nIaMtUYPK1Ec3xSdMnyAQSQO"},
+		{Name: "auth_token", Value: "0aacab7c1ab695cdf1f43296baf67f5b67a2eb83"},
+		{Name: "ct0", Value: "36c6b644b7d47c3cf0c6dd20295d2f913385b6799f068aac210b4ea3cb3edb9baac6a582acf6c3f6b64fd996a632a6d0c579631e1788fd2f3d6dcd9e5c1a887e8c282c840215a1f5fbe8f1c6405aab8b"},
+		{Name: "att", Value: "1-kEckFrFP1K3OWNp4JxbRwFxdr76AQodMj7xJyVw1"},
+		{Name: "lang", Value: "zh-cn"},
+		{Name: "twid", Value: "u%3D732225356388667392"},
+		{Name: "personalization_id", Value: "v1_NWL+26Wc/SU+2Ljoze+0gg=="},
+	}
+
+	request.SetCookies(cookies...)
+
+
+	var data = map[string]string{
+		"variables": fmt.Sprintf(`{"focalTweetId":"%s","with_rux_injections":false,"includePromotedContent":true,"withCommunity":true,"withQuickPromoteEligibilityTweetFields":true,"withBirdwatchNotes":true,"withVoice":true}`,id),
+		"features": `{"rweb_tipjar_consumption_enabled":true,"responsive_web_graphql_exclude_directive_enabled":true,"verified_phone_label_enabled":false,"creator_subscriptions_tweet_preview_api_enabled":true,"responsive_web_graphql_timeline_navigation_enabled":true,"responsive_web_graphql_skip_user_profile_image_extensions_enabled":false,"communities_web_enable_tweet_community_results_fetch":true,"c9s_tweet_anatomy_moderator_badge_enabled":true,"articles_preview_enabled":true,"tweetypie_unmention_optimization_enabled":true,"responsive_web_edit_tweet_api_enabled":true,"graphql_is_translatable_rweb_tweet_is_translatable_enabled":true,"view_counts_everywhere_api_enabled":true,"longform_notetweets_consumption_enabled":true,"responsive_web_twitter_article_tweet_consumption_enabled":true,"tweet_awards_web_tipping_enabled":false,"creator_subscriptions_quote_tweet_preview_enabled":false,"freedom_of_speech_not_reach_fetch_enabled":true,"standardized_nudges_misinfo":true,"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled":true,"rweb_video_timestamps_enabled":true,"longform_notetweets_rich_text_read_enabled":true,"longform_notetweets_inline_media_enabled":true,"responsive_web_enhance_cards_enabled":false}`,
+		"fieldToggles": `{"withArticleRichContentState":true,"withArticlePlainText":false,"withGrokAnalyze":false}`,
+	}
+	targetUrl := "https://x.com/i/api/graphql/QVo2zKMcLZjXABtcYpi0mA/TweetDetail"
+	//要把data，变成form表单的形式然后get出去
+	request.SetFormData(data)
+
+	resp, err := request.Get(targetUrl)
+	if err != nil {
+		handlerError(err)
+	}
+
+	var result = map[string]interface{}{}
+	err = resp.Unmarshal(&result)
+	if err != nil {
+		handlerError(err)
+	}
+
+	list := result["data"].(map[string]interface{})["threaded_conversation_with_injections_v2"].(map[string]interface{})["instructions"].([]interface{})[0].(map[string]interface{})["entries"].([]interface{})[0].(map[string]interface{})["content"].(map[string]interface{})["itemContent"].(map[string]interface{})["tweet_results"].(map[string]interface{})["result"].(map[string]interface{})["legacy"].(map[string]interface{})["entities"].(map[string]interface{})["media"]
+	op := list.([]interface{})[0].(map[string]interface{})["video_info"].(map[string]interface{})["variants"]
+	// 取出op中的所有url
+	resultList := []string{}
+	for _, v := range op.([]interface{}) {
+		// fmt.Println(v.(map[string]interface{})["url"])
+		if v.(map[string]interface{})["content_type"].(string) == "video/mp4" {
+			resultList = append(resultList, v.(map[string]interface{})["url"].(string))
+		}
+
+	}
+	// fmt.Println(resultList)
+	return resultList
+}
+
+func main() {
+	// 获取用户输入的推特网址
+	var url string
+	fmt.Println("请输入推特网址：")
+	fmt.Scanln(&url)
+	fmt.Println("您输入的推特网址是：", url)
+	// 获取推特id,以status/为分隔符，取最后一个
+	id := strings.Split(url, "status/")[1]
+	fmt.Println("推特id是：", id)
+	list := getInfo(id)
+	for _, v := range list {
+		fmt.Println(v)
+	}
+
+}
